40aa11fb3e0ab49098bbeac9bf922181b666ca62
Parima SANGSIRAKOUP <parima.sa@ku.th>
2024-10-14 15:29:26 +0700

    Extract Frequent Renter Points.

--- rental_test.py
+++ rental_test.py
@@ -18,7 +18,7 @@ class RentalTest(unittest.TestCase):
 		self.assertEqual(Movie.REGULAR, m.get_price_code())
 
 	def test_rental_price(self):
-		"""test get_price method in rental, check if it returns correct price."""
+		"""Test get_price method in rental, check if it returns correct price."""
 		# New Release Movie
 		rental = Rental(self.new_movie, 1)
 		self.assertEqual(rental.get_price(), 3*1)
@@ -35,6 +35,16 @@ class RentalTest(unittest.TestCase):
 		rental = Rental(self.childrens_movie, 7)
 		self.assertEqual(rental.get_price(), (1.5 + (4 * 1.5)))
 
-	@unittest.skip("add this test of rental points when you add it to Rental")
 	def test_rental_points(self):
-		self.fail("add this test of frequent renter points")
+		"""Test rental_points method in rental, check if it returns correctly."""
+		# New Release Movie
+		rental = Rental(self.new_movie, 3)
+		self.assertEqual(rental.rental_points(), 3)
+		rental = Rental(self.new_movie, 15)
+		self.assertEqual(rental.rental_points(), 15)
+		# Regular Movie
+		rental = Rental(self.regular_movie, 2)
+		self.assertEqual(rental.rental_points(), 1)
+		# Children Movie
+		rental = Rental(self.childrens_movie, 8)
+		self.assertEqual(rental.rental_points(), 1)
