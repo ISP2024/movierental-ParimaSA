6ce84497b1fc5b13a7b1de1c8536afc8b0e19656
Parima SANGSIRAKOUP <parima.sa@ku.th>
2024-10-20 18:28:21 +0700

    Modify Rental class to determine the Price Code itself

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ from pricing import ChildrenPrice, RegularPrice, NewRelease
 def make_movies():
     """Some sample movies."""
     movies = [
-        Movie("Air", 2022, ["Kid", "Adventure"]),
-        Movie("Oppenheimer", 2024, ["History", "Action"]),
-        Movie("Frozen", 2022, ["Kid", "Family"]),
-        Movie("Bitconned", 2020, ["Tech"]),
+        Movie("Air", 2024, ["Kid", "Adventure"]),
+        Movie("Oppenheimer", 2023, ["History", "Action"]),
+        Movie("Frozen", 2022, ["children", "Family"]),
+        Movie("Bitconned", 2024, ["Tech"]),
         Movie("Particle Fever", 2019, ["Action"])
     ]
     return movies
@@ -23,8 +23,7 @@ if __name__ == '__main__':
     customer = Customer("Edward Snowden")
     days = 1
     movie = make_movies()
-    price = [NewRelease, RegularPrice, ChildrenPrice]
     for n in range(len(movie)):
-        customer.add_rental(Rental(movie[n], days, price[n%3]))
+        customer.add_rental(Rental(movie[n], days))
         days = (days + 2) % 5 + 1
     print(customer.statement())
